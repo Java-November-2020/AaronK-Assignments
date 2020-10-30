83f564a75fa3c7bade7832660632c18e11677fe8
Aaron Kirchhoff <askirchhoff@gmail.com>
2020-10-29 19:11:52 -0700

    completed first assignment

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+        public static void main(String[] args) {
+            System.out.println("My name is Aaron Kirchhoff.");
+            System.out.println("I am 34 years old.");
+            System.out.println("I am from St. Louis, Missouri.");
+        }
+}
