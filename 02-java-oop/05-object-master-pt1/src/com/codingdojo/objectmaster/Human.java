9bf027213e0ca471c51c4fed7aeace301908cd8c
Aaron Kirchhoff <askirchhoff@gmail.com>
2020-11-10 11:33:45 -0800

    object master using spring suite

---
+++ 02-java-oop/05-object-master-pt1/src/com/codingdojo/objectmaster/Human.java
@@ -0,0 +1,16 @@
+package com.codingdojo.objectmaster;
+
+public class Human {
+	public int strength = 3;
+	public int stealth = 3;
+	public int intelligence =3;
+	public int health = 100;
+
+	
+	public void attack(Human target) {
+		System.out.print("Hiiiyyyaaaahhhhhh!!!");
+		target.health -= this.strength;
+		System.out.println("Current health:" + target.health);
+	}
+
+}
---
+++ 02-java-oop/05-object-master-pt1/src/com/codingdojo/objectmaster/HumanTest.java
@@ -0,0 +1,14 @@
+package com.codingdojo.objectmaster;
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Human myhuman = new Human();
+		
+		myhuman.attack(myhuman);
+		
+
+	}
+
+}
